a7595d10c1ab1eab00d41441e3d7b6c47875510f
Venkateswara Kotha <kothav4@gmail.com>
2019-08-22 12:26:39 -0400

    ROuterLinks and Params

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
@@ -11,6 +12,14 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+
+const appRoutes: Routes = [
+{path: '', component: HomeComponent },
+{path: 'servers', component: ServersComponent },
+{path: 'servers/:id/edit', component: EditServerComponent },
+{path: 'users', component: UsersComponent },
+{path: 'users/:id/:name', component: UserComponent }
+];
 
 @NgModule({
   declarations: [
@@ -25,6 +34,7 @@ import { ServersService } from './servers/servers.service';
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
